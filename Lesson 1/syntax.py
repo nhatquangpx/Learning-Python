ddc3dcce99e8a5e56ceb13ae459685d72786420f
nhatquangpx <doannhatquang0@gmail.com>
2025-07-09 18:19:54 +0700

    EX2

---
+++ Lesson 1/syntax.py
@@ -0,0 +1,18 @@
+# Hướng dẫn thụt lề
+# Các dòng lệnh trong cùng một khối lệnh phải được thụt lề giống nhau
+x = 10
+if x>5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
+
+# Biến 
+# Python không có lệnh để khai báo biến
+y = 20
+z = "hello"
+
+# Cú pháp chú thích nhiều dòng
+"""
+Đây là chú thích nhiều dòng, dùng 3 dấu nháy kép
+Có thể xuống dòng khi chú thích
+"""
